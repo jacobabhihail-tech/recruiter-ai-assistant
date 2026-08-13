bbd0a8247187ecb5487aa28100eb62b1dc845502
jacobabhihail-tech <jacob.abhihail@gmail.com>
2026-08-13 16:19:50 +0530

    Folder and files initialized

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Recruitment Assistant",
+    page_icon="🤖",
+)
+
+st.title("🤖 AI Recruitment Assistant")
+
+st.write(
+    "Hi! I’m your AI Recruitment Assistant. "
+    "Upload your resume to get started."
+)
